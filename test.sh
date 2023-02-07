efa4b1a918705b5339ff1b14f576d379212a41d9
Ryan Raymond <rjraymond@oakland.edu>
2023-02-07 12:20:02 -0500

    Added test thingy

--- test.sh
+++ test.sh
@@ -3,5 +3,11 @@
 killall node
 killall php
 (cd src;  php -S localhost:8000 )&
-FA_LOCAL=true BROWSER=none exec npm start &
+REACT_APP_LOCAL="true" \
+REACT_APP_URL="localhost:3000" \
+REACT_APP_PWD="./" \
+REACT_APP_PHP="./" \
+REACT_APP_URL_PHP="localhost:8000" \
+BROWSER=none \
+exec npm start &
 firefox http://localhost:3000
